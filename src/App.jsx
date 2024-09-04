c046f75bdf0183f1d547dbb467f1d1f32f9cd6dd
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-04 17:05:41 +0200

    feat: improved context

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,27 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-            <Accordion.Item id="experience" className="accordion-item" title="We got 20 years of experience">
-                <article>
-                    <p>You can't go wrong with us</p>
-                    <p>We are in the business.......</p>
-                </article>
+            <Accordion.Item id="experience" className="accordion-item">
+                <Accordion.Title className="accordion-item-title">
+                    We got 20 years of experience
+                </Accordion.Title>
+                <Accordion.Content className="accordion-item-content">
+                    <article>
+                        <p>You can't go wrong with us</p>
+                        <p>We are in the business.......</p>
+                    </article>
+                </Accordion.Content>
             </Accordion.Item>
-            <Accordion.Item id="local-guide"  className="accordion-item" title="We're working with local guides">
-                <article>
-                    <p>We are not doing along...</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, et.</p>
-                </article>
+            <Accordion.Item id="local-guide"  className="accordion-item">
+                <Accordion.Title className="accordion-item-title">
+                    We're working with local guides
+                </Accordion.Title>
+                <Accordion.Content className="accordion-item-content">
+                    <article>
+                        <p>We are not doing along...</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, et.</p>
+                    </article>
+                </Accordion.Content>
             </Accordion.Item>
         </Accordion>
       </section>
---
+++ src/components/Accordion/AccordionContent.jsx
@@ -0,0 +1,15 @@
+import { useAccordionContext } from "./Accordion";
+import { useAccordionItemContext } from "./AccordionItem";
+
+export default function AccordionContent({ className, children}) {
+    const {openItemId} = useAccordionContext();
+    const id = useAccordionItemContext();
+
+    const isOpen = openItemId === id;
+
+    return (
+        <li className={className}>
+            <div className={isOpen ? `${className ?? ''} open` : `${className ?? ''} close`}>{children}</div>
+        </li>
+    )
+}
